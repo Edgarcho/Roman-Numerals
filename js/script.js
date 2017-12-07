07eb8094dd6f16b7a6b3613515fd7b06dd5ad59f
Edgar Hernandez and Omar Solano <omar@>
2017-12-07 13:22:34 -0800

    add  function to back-end in js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ function convertRoman(number){
    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
    var letters = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
    var result = '';
-    if(number < 4000){  // run the if statement if number is less 4000 
-      for (var i = 0; i < numbers.length; i++){
-        while (number >= numbers[i]){
-          result += letters[i]
-          number -= numbers[i];
+    if(number < 4000){  // run the if statement if number is less than 4000
+      for (var i = 0; i < numbers.length; i++){ // run the for loop till i is less than number length. (13 in length)
+        while (number >= numbers[i]){ // also in the for loop it must meet the while loop condition. Ex(number = 8 and i = 10 aka V (8 >= numbers[10]))
+          result += letters[i] // letter is V in index 10 is added into result (Result:V)
+          number -= numbers[i]; //the remaining number is insert in the function again and repeat the process till the condition is not meet.
         }
       }
-      return result;
+      return result; //return result back to user inferface.
     }else{
       result = "We cannot count to 4000 in Roman numeral";
       return result;
